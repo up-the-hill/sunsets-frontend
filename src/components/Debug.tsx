c96ac57724001bb314c4af5f6901aae46e21a790
up-the-hill <aj.sth.2022@gmail.com>
2025-12-27 15:27:23 +1100

    improved debug logic

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -6,13 +6,13 @@ interface DebugProps {
   map: Map | null;
 }
 
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+function getDistanceFromLatLonInKm(pos1: LngLat, pos2: LngLat) {
   const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1);  // deg2rad below
-  const dLon = deg2rad(lon2 - lon1);
+  const dLat = deg2rad(pos2.lat - pos1.lat);  // deg2rad below
+  const dLon = deg2rad(pos2.lng - pos1.lng);
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.cos(deg2rad(pos1.lat)) * Math.cos(deg2rad(pos2.lat)) *
     Math.sin(dLon / 2) * Math.sin(dLon / 2)
     ;
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
@@ -20,7 +20,7 @@ function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
   return d;
 }
 
-function deg2rad(deg) {
+function deg2rad(deg: number) {
   return deg * (Math.PI / 180)
 }
 
@@ -65,7 +65,7 @@ export default function Debug({ map }: DebugProps) {
         <summary>Bounds</summary>
         <p>sw: {bounds?._sw.toString() ?? 'loading'}</p>
         <p>ne: {bounds?._ne.toString() ?? 'loading'}</p>
-        <p>distance: {getDistanceFromLatLonInKm(bounds?._sw.lat, bounds?._sw.lng, bounds?._ne.lat, bounds?._ne.lng)}</p>
+        <p>distance: {getDistanceFromLatLonInKm(bounds._ne, bounds._sw)} km</p>
       </details>
     </div>
   )
