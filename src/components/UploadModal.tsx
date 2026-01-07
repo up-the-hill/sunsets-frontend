3f8c27835abf1a1483dec2221584460da68a476b
up-the-hill <aj.sth.2022@gmail.com>
2026-01-08 10:50:59 +1100

    integrated tfjs model support

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -4,73 +4,90 @@ import Compressor from 'compressorjs';
 
 export default function UploadModal({ handleCloseModal, clickMarker }) {
   const fileRef = useRef<HTMLInputElement | null>(null);
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     const input = fileRef.current;
     const file = input!.files![0];
 
-    // API Call to get Presigned POST data
-    const formData = new FormData();
-    if (clickMarker) {
-      const { lng, lat } = clickMarker.getLngLat();
-      formData.append('longitude', lng.toPrecision(8));
-      formData.append('latitude', lat.toPrecision(8));
-    }
+    // compress image for 1st upload
+    new Compressor(file, {
+      quality: 0.8,
+      maxHeight: 224,
+      async success(result) {
 
-    try {
-      const res = await fetch('/api/sunsets', {
-        method: 'POST',
-        body: formData
-      });
+        // API Call to get Presigned POST data, and check if image is a sunset
+        const formData = new FormData();
+        if (clickMarker) {
+          const { lng, lat } = clickMarker.getLngLat();
+          formData.append('longitude', lng.toPrecision(8));
+          formData.append('latitude', lat.toPrecision(8));
+          formData.append("file", result);
+        }
 
-      if (!res.ok) {
-        throw new Error('Failed to get upload parameters');
-      }
-
-      const { url, fields } = await res.json();
-
-
-      new Compressor(file, {
-        quality: 0.7,
-        maxWidth: 1080,
-
-        // The compression process is asynchronous,
-        // which means you have to access the `result` in the `success` hook function.
-        async success(result) {
-          // Construct FormData for S3 upload
-          const s3FormData = new FormData();
-          Object.entries(fields).forEach(([key, value]) => {
-            s3FormData.append(key, value as string);
-          });
-          s3FormData.append('file', result);
-
-          // Upload to S3
-          const uploadRes = await fetch(url, {
+        try {
+          const res = await fetch('/api/sunsets', {
             method: 'POST',
-            body: s3FormData
+            body: formData
           });
 
-          if (!uploadRes.ok) {
-            if (uploadRes.status === 400 && (await uploadRes.text()).includes('EntityTooLarge')) {
-              alert("filesize too large!");
-              throw new Error('File too large (max 5MB)');
+          if (!res.ok) {
+            if (res.status === 400 && (await res.text()) == 'ImageNotSunset') {
+              // alert("Image not Sunset!");
+              throw new Error('Image not Sunset!');
             }
-            alert("upload failed!");
-            throw new Error('Upload to S3 failed');
-          } else {
-            alert("image uploaded!");
+            throw new Error('Failed to get upload parameters');
           }
-          handleCloseModal();
-        },
-        error(err) {
-          console.log(err.message);
-        },
-      });
-    } catch (err) {
-      console.error(err);
-      alert(err instanceof Error ? err.message : "Upload failed");
-    }
+
+          const { url, fields } = await res.json();
+
+          // compress the image for upload to s3 bucket
+          new Compressor(file, {
+            quality: 0.7,
+            maxWidth: 1080,
+
+            async success(result) {
+              // Construct FormData for S3 upload
+              const s3FormData = new FormData();
+              Object.entries(fields).forEach(([key, value]) => {
+                s3FormData.append(key, value as string);
+              });
+              s3FormData.append('file', result);
+
+              // Upload to S3
+              const uploadRes = await fetch(url, {
+                method: 'POST',
+                body: s3FormData
+              });
+
+              if (!uploadRes.ok) {
+                if (uploadRes.status === 400 && (await uploadRes.text()).includes('EntityTooLarge')) {
+                  alert("filesize too large!");
+                  throw new Error('File too large (max 5MB)');
+                }
+                alert("upload failed!");
+                throw new Error('Upload to S3 failed');
+              } else {
+                alert("image uploaded!");
+              }
+              handleCloseModal();
+            },
+            error(err) {
+              console.log(err.message);
+            },
+          });
+        } catch (err) {
+          console.error(err);
+          alert(err instanceof Error ? err.message : "Upload failed");
+        }
+      },
+      error(e) {
+        console.error(e)
+      },
+    })
+
+
   }
 
   return (
